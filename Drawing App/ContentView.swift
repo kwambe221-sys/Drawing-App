7cf04ec929c329c4a786934415ae070c4a6e5d3a
Kwame Boateng <Kwambe221@gmail.com>
2026-05-01 21:14:44 -0500

    Initial Commit

---
+++ Drawing App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Drawing App
+//
+//  Created by Boateng, Kwame on 5/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Drawing App/Drawing_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_AppApp.swift
+//  Drawing App
+//
+//  Created by Boateng, Kwame on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
